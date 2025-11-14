be80c3a27283989cabc06ef3efa49eb87b9c582f
luthfi <luthfiardhiansyah590@gmail.com>
2025-11-14 10:50:04 +0700

    tugas assesment akhir

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name'=>'Luthfi',
+            'email'=>'lutpistangseher@gmail.com',
+            'password'=> bcrypt('123456789')
+        ]);
+    }
+}
